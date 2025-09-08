c2a44e96b63baba33345e48e6f24b6da61ec1c76
Jerit Baiju <jeritalumkal@gmail.com>
2025-09-08 21:14:39 +0530

    feat(models): add College model with fields for name, domain, and time windows

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class College(models.Model):
+    name = models.CharField(max_length=255)
+    domain = models.CharField(max_length=255, unique=True)
+    window_start = models.TimeField()
+    window_end = models.TimeField()
+
+    def __str__(self):
+        return f"{self.name}"
