43ba78e4cd825fc041252c6a686e531906fe0202
Jerit Baiju <jeritalumkal@gmail.com>
2025-09-09 16:37:42 +0530

    refactor(admin): clean up imports and ensure model registration is clear

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import College, WaitingListEntry
 
 # Register your models here.
+
+admin.site.register(College)
+admin.site.register(WaitingListEntry)
